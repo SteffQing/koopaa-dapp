e830dd9085b5d7c4d7d6a753d0ba3cacca71bac5
Steve Tomi <steveola23@gmail.com>
2025-05-09 00:31:52 +0100

    remove unused Image import from members page

--- src/app/(mobile-ui)/savings/ajo/[id]/members/page.tsx
+++ src/app/(mobile-ui)/savings/ajo/[id]/members/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import Image from 'next/image'
 import NavHeader from '@/views/Navigation/nav-header'
 import Container from '@/components/container'
 import { use } from 'react'
