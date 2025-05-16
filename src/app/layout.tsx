f0ea763c944b5c9ecf95122b4a553ebe3c87243f
Steve Tomi <steveola23@gmail.com>
2025-05-16 12:48:45 +0100

    add toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,33 @@
-import type { Metadata } from 'next'
-import './globals.css'
-// import { AppLayout } from '@/components/app-layout'
-import React from 'react'
-import { AppProviders } from '@/providers'
+import type { Metadata } from "next";
+import "./globals.css";
+import React from "react";
+import { AppProviders } from "@/providers";
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-  title: 'KooPaa',
-  description: 'Save Smart with Your Circle, On-Chain',
-}
+  title: "KooPaa",
+  description: "Save Smart with Your Circle, On-Chain",
+};
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`antialiased`}>
         <AppProviders>{children}</AppProviders>
+        <Toaster />
       </body>
     </html>
-  )
+  );
 }
 // Patch BigInt so we can log it using JSON.stringify without any errors
 declare global {
   interface BigInt {
-    toJSON(): string
+    toJSON(): string;
   }
 }
 
 BigInt.prototype.toJSON = function () {
-  return this.toString()
-}
+  return this.toString();
+};
