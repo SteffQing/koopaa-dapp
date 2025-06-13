4be65db87e1d93b4a74023590bf7be1530328465
Steve Tomi <steveola23@gmail.com>
2025-06-13 18:34:42 +0100

    add tg link

--- src/app/(mobile-ui)/template.tsx
+++ src/app/(mobile-ui)/template.tsx
@@ -3,16 +3,16 @@ import { Suspense, ReactNode } from "react";
 import AuthGuard from "./AuthGuard";
 import SplashScreen from "@/views/splash-screen";
 import Faucet from "@/components/faucet";
+import Telegram from "@/components/telegram";
 
-export default function MobileLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
+export default function MobileLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <Suspense fallback={<SplashScreen />}>
       <AuthGuard>
         {children}
         <BottomNavbar />
         <Faucet />
+        <Telegram />
       </AuthGuard>
     </Suspense>
   );
