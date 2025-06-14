276cf89278f7b8544c37edfa9421d9de283dbf8e
Steve Tomi <steveola23@gmail.com>
2025-06-14 09:30:13 +0100

    fix builkd error

--- src/components/telegram.tsx
+++ src/components/telegram.tsx
@@ -2,36 +2,30 @@
 
 import { motion, AnimatePresence } from "framer-motion";
 import TelegramIcon from "@/assets/svgs/telegram.svg";
-import { X } from "lucide-react";
-import { useLocalStorage } from "@/hooks/useLocalStorage";
 import Link from "next/link";
 
 export default function Telegram() {
-  const [isVisible, setIsVisible] = useLocalStorage("telegram", true);
-
   return (
     <AnimatePresence>
-      {isVisible && (
-        <motion.div
-          className="fixed bottom-6 left-1/2 transform -translate-x-1/2 flex gap-6 items-center mx-auto max-w-[400px] w-full"
-          initial={{ y: 100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: 100, opacity: 0 }}
-          transition={{
-            delay: 0.9,
-            stiffness: 500,
-            damping: 30,
-          }}
-        >
-          <div className="max-w-[400px] bg-transparent absolute bottom-[140px] right-4">
-            <div className="relative group">
-              <Link href="https://t.me/+hxd5Ob1F78hkZTNk" target="_blank">
-                <TelegramIcon />
-              </Link>
-            </div>
+      <motion.div
+        className="fixed bottom-6 left-1/2 transform -translate-x-1/2 flex gap-6 items-center mx-auto max-w-[400px] w-full"
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 100, opacity: 0 }}
+        transition={{
+          delay: 0.9,
+          stiffness: 500,
+          damping: 30,
+        }}
+      >
+        <div className="max-w-[400px] bg-transparent absolute bottom-[140px] right-4">
+          <div className="relative group">
+            <Link href="https://t.me/+hxd5Ob1F78hkZTNk" target="_blank">
+              <TelegramIcon />
+            </Link>
           </div>
-        </motion.div>
-      )}
+        </div>
+      </motion.div>
     </AnimatePresence>
   );
 }
