7abc78c15d6993b747cdb7e5057c244d012d5926
Steve Tomi <steveola23@gmail.com>
2025-09-18 01:28:12 +0100

    make async

--- src/actions/tg.ts
+++ src/actions/tg.ts
@@ -3,14 +3,14 @@
 const TG_CHAT = process.env.TG_CHAT;
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
-export default function sendMessage(text: string) {
+export default async function sendMessage(text: string) {
   const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
   const body = JSON.stringify({
     chat_id: TG_CHAT,
     text: text,
   });
 
-  return fetch(url, {
+  await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
